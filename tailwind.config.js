c80f2d4a83cf9f3f915f11fbd2c17330b77bf99a
Pedro-Estevao <contato@pedroestevao.com>
2023-10-13 17:43:40 -0300

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,43 +10,7 @@ const tailwindConfig = {
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
 	],
-	theme: {
-		extend: {
-			backgroundColor: {
-				light: '#f3f3f3',
-				dark: '#0b0b0c'
-			},
-			spacing: {
-				18: '4.5rem'
-			},
-			fontFamily: {
-				sans: ['"Poppins"', 'sans-serif']
-			},
-			colors: {
-				blue: {
-					600: '#006FF9'
-				}
-			}
-		},
-		screens: {
-			xs: '375px',
-			sm: '576px',
-			md: '768px',
-			lg: '992px',
-			xl: '1200px',
-			xxl: '1400px',
-			'max-xs': { max: '374px' },
-			'max-sm': { max: '575px' },
-			'max-md': { max: '767px' },
-			'max-lg': { max: '991px' },
-			'max-xl': { max: '1199px' },
-			'max-xxl': { max: '1399px' }
-		},
-		logo: {
-			light: '#FAFAFA',
-			dark: '#0A0D27'
-		}
-	},
+	theme: {},
 	plugins: [
 		nextui({
 			prefix: 'nextui',
