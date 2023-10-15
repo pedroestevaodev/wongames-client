6d2b89a9f8a5432bbabc472891edb3f8561388f8
Pedro-Estevao <contato@pedroestevao.com>
2023-10-15 11:31:22 -0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import Main from '@/components/Main';
+import ThemeToggle from '@/components/ThemeToggle';
+import Heading from '@/components/Heading';
+import Logo from '@/components/Logo';
 
 const Home = () => {
-	return <Main />;
+	return (
+		<>
+			<Main />
+			<ThemeToggle />
+			<Logo hideOnMobile color="black" />
+			<Heading color="black">
+				<span>Teste</span>
+			</Heading>
+		</>
+	);
 };
 
 export default Home;
