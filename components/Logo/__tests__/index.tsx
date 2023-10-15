b465507bd244c636d442a141e524ee0fb188da43
Pedro-Estevao <contato@pedroestevao.com>
2023-10-15 14:55:41 -0300

    Update Logo component

--- components/Logo/__tests__/index.tsx
+++ components/Logo/__tests__/index.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Logo from '@/components/Logo';
+// import 'jest-styled-components';
 
 describe('<Logo />', () => {
 	it('should render a white label by default', () => {
-		render(<Logo hideOnMobile />);
+		render(<Logo />);
 
 		expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
 			color: 'rgb(250 250 250 / var(--tw-text-opacity))'
@@ -11,38 +12,35 @@ describe('<Logo />', () => {
 	});
 
 	it('should render a black label when theme is dark', () => {
-		render(<Logo />);
+		render(<Logo color="black" />);
 
 		expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-			color: 'rgb(10 13 39 / var(--tw-text-opacity))'
+			color: 'rgb(3 5 23 / var(--tw-text-opacity))'
 		});
 	});
 
-	it('should render a normal logo when size is default', () => {
-		render(<Logo />);
+	// it('should render a normal logo when size is default', () => {
+	// 	render(<Logo />);
 
-		expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-			width: '11rem'
-		});
-	});
+	// 	expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+	// 		width: '11rem'
+	// 	});
+	// });
 
-	it('should render a bigger logo', () => {
-		render(<Logo size="large" />);
+	// it('should render a bigger logo', () => {
+	// 	render(<Logo size="large" />);
 
-		expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-			width: '20rem'
-		});
-	});
+	// 	expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+	// 		width: '20rem'
+	// 	});
+	// });
 
-	// it('should render a logo without text if hideOnMobile', () => {
+	// Conflict
+	// it('should render a bigger logo without text if hideOnMobile', () => {
 	//     render(<Logo hideOnMobile />);
 
-	//     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
-	//         'width',
-	//         '5.8rem',
-	//         {
-	//             media: '(max-width: 768px)'
-	//         }
-	//     );
+	//     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule('width', '5.8rem', {
+	//         media: '(max-width: 768px)'
+	//     });
 	// });
 });
--- components/Logo/styles/index.ts
+++ components/Logo/styles/index.ts
@@ -1,38 +1,6 @@
 'use client';
 
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { LogoProps } from '..';
-import media from 'styled-media-query';
 
-const containerModifiers = {
-	normal: () => css`
-		width: 11rem;
-		height: 3.3rem;
-	`,
-	large: () => css`
-		width: 20rem;
-		height: 5.2rem;
-	`,
-	hideOnMobile: () => css`
-		${media.lessThan('medium')`
-            width: 5.8rem;
-            height: 4.5rem;
-
-            svg {
-                height: 4.5rem;
-                pointer-events: none;
-            }
-
-            .logo-text {
-                display: none;
-            }
-        `}
-	`
-};
-
-export const LogoContainer = styled.div<LogoProps>`
-	${({ size, hideOnMobile }) => css`
-		${!!size && containerModifiers[size]}
-		${!!hideOnMobile && containerModifiers.hideOnMobile}
-	`}
-`;
+export const LogoContainer = styled.div<LogoProps>``;
