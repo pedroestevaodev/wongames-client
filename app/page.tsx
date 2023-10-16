f2f9d20943922b353d0c28b1a885a98c234d4207
Pedro-Estevao <contato@pedroestevao.com>
2023-10-15 23:19:36 -0300

    Update project

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Main from '@/components/Main';
 import ThemeToggle from '@/components/ThemeToggle';
-import Heading from '@/components/Heading';
 import Logo from '@/components/Logo';
+import Heading from '@/components/Heading';
+import Button from '@/components/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 	return (
@@ -13,6 +16,9 @@ const Home = () => {
 			<Heading color="black" lineLeft>
 				Won Games
 			</Heading>
+			<Button size="large" icon={<FontAwesomeIcon icon={faCartArrowDown} />}>
+				Buy now
+			</Button>
 		</>
 	);
 };
