94a2cf8d8b6867da7edbf905cc7a9230e08c15c1
Pedro Estevão <contato@pedroestevao.com>
2023-10-21 15:07:10 -0300

    Create BannerSlider component

---
+++ components/BannerSlider/stories/index.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/react';
+import BannerSlider, { BannerSliderProps } from '@/components/BannerSlider';
+
+const items = [
+	{
+		img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+		title: 'Defy death 1',
+		subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+		buttonLabel: 'Buy now',
+		buttonLink: '/games/defy-death',
+		ribbon: 'Bestselling'
+	},
+	{
+		img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+		title: 'Defy death 2',
+		subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+		buttonLabel: 'Buy now',
+		buttonLink: '/games/defy-death'
+	},
+	{
+		img: 'https://source.unsplash.com/user/willianjusten/1042x581',
+		title: 'Defy death 3',
+		subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+		buttonLabel: 'Buy now',
+		buttonLink: '/games/defy-death'
+	}
+];
+
+export default {
+	title: 'BannerSlider',
+	component: BannerSlider,
+	argTypes: {},
+	args: {
+		items
+	},
+	parameters: {
+		layout: 'fullscreen',
+		backgrounds: {
+			default: 'dark'
+		}
+	}
+} as Meta;
+
+export const Default: StoryObj<BannerSliderProps> = {
+	decorators: [
+		(Story) => (
+			<div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+				<Story />
+			</div>
+		)
+	]
+};
