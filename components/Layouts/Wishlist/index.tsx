9a5f6703319ce721ec58778926112af61c1c1be2
Pedro-Estevao <contato@pedroestevao.com>
2024-02-17 17:04:44 -0300

    Update Wishlist layout

--- components/Layouts/Wishlist/index.tsx
+++ components/Layouts/Wishlist/index.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
-// import * as S from "./styles";
 import Base from '../Base';
 import Heading from '@/components/Heading';
 import Container from '@/components/Container';
+import GameCard, { GameCardProps } from '@/components/GameCard';
+import { HighlightProps } from '@/components/Highlight';
+import ShowCase from '@/components/ShowCase';
+import Grid from '@/components/Grid';
+import Divider from '@/components/Divider';
+import Empty from '@/components/Empty';
 
-const Wishlist = () => {
+export type WishlistLayoutProps = {
+	games?: GameCardProps[];
+	recommendedGames: GameCardProps[];
+	recommendedHighlight: HighlightProps;
+};
+
+const Wishlist = ({
+	games = [],
+	recommendedGames,
+	recommendedHighlight
+}: WishlistLayoutProps) => {
 	return (
 		<Base>
 			<Container>
 				<Heading lineLeft lineColor="secondary">
 					Wishlist
 				</Heading>
+
+				{games.length ? (
+					<Grid>
+						{games?.map((game, index) => (
+							<GameCard key={`wishlist-${index}`} {...game} />
+						))}
+					</Grid>
+				) : (
+					<Empty
+						title="Your wishlist is empty"
+						description="Games added to your wishlist will appear here"
+						hasLink
+					/>
+				)}
+
+				<Divider />
 			</Container>
+
+			<ShowCase
+				title="You may like these games"
+				games={recommendedGames}
+				highlight={recommendedHighlight}
+			/>
 		</Base>
 	);
 };
