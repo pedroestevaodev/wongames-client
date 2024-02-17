3371b39b133b541cbc6b05e9b0bcf5af6b89c153
Pedro-Estevao <contato@pedroestevao.com>
2024-02-17 17:07:02 -0300

    Update wishlist page

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,7 +1,25 @@
-import Wishlist from '@/components/Layouts/Wishlist';
+import React from 'react';
+import Wishlist, { WishlistLayoutProps } from '@/components/Layouts/Wishlist';
+import wishlistLayoutMock from '@/components/Layouts/Wishlist/mocks/mock';
 
-const WishlistPage = () => {
-	return <Wishlist />;
+type WishlistPageProps = {
+	props: WishlistLayoutProps;
+	params: {
+		slug: string;
+	};
+};
+
+const WishlistPage = ({ props, params }: WishlistPageProps) => {
+	console.log(params);
+	console.log(props);
+
+	return (
+		// <Wishlist {...props} />
+		<Wishlist
+			recommendedGames={wishlistLayoutMock.recommendedGames}
+			recommendedHighlight={wishlistLayoutMock.recommendedHighlight}
+		/>
+	);
 };
 
 export default WishlistPage;
