4d4cb411f6ac5e8357a16e3b185aed1176edf8b9
Pedro-Estevao <contato@pedroestevao.com>
2024-02-18 11:43:36 -0300

    Create ProfileMenu component

---
+++ components/ProfileMenu/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import * as S from './styles';
+import {
+	RiAccountCircleLine,
+	RiBankCard2Line,
+	RiListUnordered,
+	RiLogoutCircleRLine
+} from '@remixicon/react';
+
+export type ProfileMenuProps = {
+	activeLink?: string;
+};
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => {
+	return (
+		<S.ProfileMenuContainer>
+			<S.Menu
+				href="/profile/me"
+				isActive={activeLink === '/profile/me'}
+				title="My profile"
+			>
+				<RiAccountCircleLine size={24} />
+				<S.MenuLabel>My profile</S.MenuLabel>
+			</S.Menu>
+
+			<S.Menu
+				href="/profile/cards"
+				isActive={activeLink === '/profile/cards'}
+				title="My cards"
+			>
+				<RiBankCard2Line size={24} />
+				<S.MenuLabel>My cards</S.MenuLabel>
+			</S.Menu>
+
+			<S.Menu
+				href="/profile/orders"
+				isActive={activeLink === '/profile/orders'}
+				title="My orders"
+			>
+				<RiListUnordered size={24} />
+				<S.MenuLabel>My orders</S.MenuLabel>
+			</S.Menu>
+
+			<S.Menu
+				href="/logout"
+				isActive={activeLink === '/logout'}
+				title="Sign out"
+			>
+				<RiLogoutCircleRLine />
+				<S.MenuLabel>Sign out</S.MenuLabel>
+			</S.Menu>
+		</S.ProfileMenuContainer>
+	);
+};
+
+export default ProfileMenu;
---
+++ components/ProfileMenu/styles/index.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+export const ProfileMenuContainer = styled.nav`
+	display: flex;
+	border-bottom: 0.1rem solid #eaeaea;
+
+	@media (min-width: 768px) {
+		flex-direction: column;
+		border: 0;
+
+		a:not(:last-child) {
+			border-bottom: 0.1rem solid #eaeaea;
+		}
+	}
+`;
+
+const menuModifiers = {
+	default: () => `
+        background-color: #FAFAFA;
+        color: #030517;
+    `,
+	active: () => `
+        background-color: #F231A5;
+        color: #FAFAFA;
+    `
+};
+
+type MenuProps = {
+	isActive?: boolean;
+};
+
+export const Menu = styled(Link)<MenuProps>`
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding: 1.6rem 2.4rem;
+	transition:
+		background-color,
+		color,
+		0.3s ease-in-out;
+
+	&:hover {
+		background: #f231a5;
+		color: #fafafa;
+	}
+
+	> span {
+		margin-left: 1.6rem;
+	}
+
+	@media (max-width: 767px) {
+		justify-content: center;
+		flex: 1;
+
+		> span {
+			display: none;
+		}
+	}
+
+	${({ isActive }) =>
+		isActive ? menuModifiers.active() : menuModifiers.default()}
+`;
+
+export const MenuLabel = styled.span``;
