5100c7fb1f17dfb12c750a980d8df634b25f784a
Pedro Estevão <contato@pedroestevao.com>
2024-03-07 21:19:01 -0300

    Update Providers

--- components/Providers/index.tsx
+++ components/Providers/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import GlobalStyles from "@/public/styles/global";
-import ApolloProviderWrapper from "./ApolloProvider";
-import NextUIProviderWrapper from "./NextUIProvider";
-import ThemeProviderWrapper from "./ThemeProvider";
+import ApolloProviderWrapper from "@/components/Providers/apolloProvider";
+import NextUIProviderWrapper from "@/components/Providers/nextUIProvider";
+import ThemeProviderWrapper from "@/components/Providers/themeProvider";
 
 export function Providers({ children }: React.PropsWithChildren) {
 	return (
