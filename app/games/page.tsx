bd602c28901dfcb4e3c18292a6c830e192265673
Pedro Estevão <contato@pedroestevao.com>
2024-03-28 07:47:37 -0300

    Get Games with useQuery

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Games from '@/components/Layouts/Games';
 import { getClient } from '@/lib/apolloClient';
-import { Query } from '@/graphql/generated/graphql';
+import { GameEntityResponseCollection, GetGamesQuery, GetGamesQueryVariables } from '@/graphql/generated/graphql';
 import { GET_GAMES } from '@/graphql/queries/games';
-import { GameCardProps } from '@/components/GameCard';
 import exploreSidebarMock from '@/components/ExploreSidebar/mocks/mock';
+import { gamesMapper } from "@/utils/mappers";
 
 const GamesPage = async () => {
-	const { data } = await getClient().query<Query>({
+	const { data } = await getClient().query<GetGamesQuery, GetGamesQueryVariables>({
 		query: GET_GAMES,
-		variables: { limit: 9 },
+		variables: { limit: 15 },
 		context: {
 			fetchOptions: {
 				next: { revalidate: 60 }
@@ -17,17 +17,9 @@ const GamesPage = async () => {
 		}
 	});
 
-	const gamesData = data.games?.data.map((game) => ({
-		slug: game.attributes?.slug,
-		title: game.attributes?.name,
-		developer: game.attributes?.developers?.data[0].attributes?.name,
-		img: `${process.env.API_URL}${game.attributes?.cover?.data?.attributes?.url}`,
-		price: game.attributes?.price || 0
-	}));
-
 	return (
 		<Games
-			games={gamesData as GameCardProps[]}
+			games={gamesMapper(data.games as GameEntityResponseCollection)}
 			filterItems={exploreSidebarMock}
 			// {...data as GamesLayoutProps}
 		/>
--- components/Layouts/Games/index.tsx
+++ components/Layouts/Games/index.tsx
@@ -7,13 +7,20 @@ import GameCard, { GameCardProps } from '@/components/GameCard';
 import ExploreSidebar, { ItemProps } from '@/components/ExploreSidebar';
 import Grid from '@/components/Grid';
 import { RiArrowDownSLine } from '@remixicon/react';
+import { useQuery } from "@apollo/client";
+import { GetGamesQuery, GetGamesQueryVariables } from "@/graphql/generated/graphql";
+import { GET_GAMES } from "@/graphql/queries/games";
 
 export type GamesLayoutProps = {
 	games?: GameCardProps[];
 	filterItems: ItemProps[];
 };
 
-const Games = ({ filterItems, games = [] }: GamesLayoutProps) => {
+const Games = ({ filterItems }: GamesLayoutProps) => {
+	const { loading, error, data } = useQuery<GetGamesQuery, GetGamesQueryVariables>(GET_GAMES, { 
+		variables: { limit: 15 }, 
+	});
+
 	const handleFilter = () => {
 		return;
 	};
@@ -22,15 +29,32 @@ const Games = ({ filterItems, games = [] }: GamesLayoutProps) => {
 		return;
 	};
 
+	if (loading) return <p>Loading...</p>;
+	if (error) return null;
+
+	console.log(data);
+
 	return (
 		<Base>
+			<code>
+				<pre>
+					{JSON.stringify(data, null, 2)}
+				</pre>
+			</code>
 			<S.Main>
 				<ExploreSidebar items={filterItems} onFilter={handleFilter} />
 
 				<section>
 					<Grid>
-						{games.map((item) => (
-							<GameCard key={item.title} {...item} />
+						{(data?.games as GameCardProps[] | undefined)?.map((game) => (
+							<GameCard 
+								key={game.title}
+								title={game.title}
+								slug={game.slug}
+								developer={game.developer}
+								img={`${process.env.API_URL}${game.img}`}
+								price={game.price}
+							/>
 						))}
 					</Grid>
 
